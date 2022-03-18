92616285d0cdaa38a927c46b548ab65626f3a194
Drivdmal <drivdmal@gmail.com>
2022-03-19 00:33:22 +0300

    '19_03_22'

--- matrix.py
+++ matrix.py
@@ -1,10 +1,12 @@
+# диагонали квадратных матриц
+
 matrix_5x5 = [
         [ 1, 2, 3, 4, 5],
         [ 6, 7, 8, 9,10],
         [11,12,13,14,15],
         [16,17,18,19,20],
-        [21,22,23,24,25]
-             ] 
+        [21,22,23,24,25] ] 
+
 n = len(matrix_5x5)
 diagonal_main = []
 diagonal_not_main = []
@@ -17,3 +19,19 @@ for i in range(5):
             diagonal_not_main.append(matrix_5x5[i][j])
         
 print(diagonal_main, diagonal_not_main)
+# --------------------------------------------------------------------------
+
+# make matrix n_x_m
+
+n = 4 		# index
+m = 6		# column
+
+lst = []
+matrix = []
+
+for i in range(n):
+    for j in range(m):
+       lst.append()
+    matrix.append(lst[:])
+    lst.clear()
+
